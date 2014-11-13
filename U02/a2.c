3fcc7697eecf2f6ce7334cbcc71270e5d6f3f7e5
KaosK <sven.klaus@me.com>
2014-11-13 22:03:03 +0100

    TI3 Uebung2 Aufgabe2

--- U02/a2.c
+++ U02/a2.c
@@ -7,9 +7,19 @@ char txt[100];
 
 int main(int argc, char const *argv[])
 {
-	printf("Enter a Passphrase: ")
-	scanf("%s", pw);
+	printf("Enter a Passphrase: ");
+	// scanf("%s", pw);
+
+	char text[100];
+	fgets(text, 99, stdin);
+
+	printf("%s", text);
+	printf("%s", text);
+	printf("%s", text);
+	printf("%s", text);
 
 	return 0;
 }
+
+char encode(char char, int index)
 
