347a99836a1780b7ca05c5a8958e7375526adb17
MrLoh <flojoto@hotmail.com>
2014-12-05 12:35:54 +0100

    implemented cat and grep

--- U07/a3/cat.c
+++ U07/a3/cat.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-#define BUFF_SIZE 100
+#define BUFF_SIZE 512
 
 void write_out(FILE* file) {
-	while ( !feof(file) )
+	if ( file )
 	{
-		char buffer[BUFF_SIZE];
-		size_t read = fread(buffer, 1, BUFF_SIZE, file);
-		size_t written = fwrite(buffer, 1, read, stdout);
+		while ( !feof(file) )
+		{
+			char buffer[BUFF_SIZE];
+			size_t read = fread(buffer, 1, BUFF_SIZE, file);
+			fwrite(buffer, 1, read, stdout);
+		}
 	}
+	else
+		fputs("ERROR: no input given\n", stderr);
 }
 
 int main(int argc, char const *argv[])
@@ -19,9 +24,10 @@ int main(int argc, char const *argv[])
 	}
 	else
 	{
-		// input in argv[1]
+		// filename in argv[1]
 		FILE *file = fopen(argv[1],"r");
 		write_out(file);
+		fclose(file);
 	}
 	return 0;
 }
--- U07/a3/grep.c
+++ U07/a3/grep.c
@@ -1,7 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_LINE_SIZE 2048
+
+void write_grepped(FILE* file, const char* key) {
+	if ( file )
+	{
+		while ( !feof(file) )
+		{
+			char buffer[MAX_LINE_SIZE];
+			fgets(buffer, MAX_LINE_SIZE, file);
+			if ( strstr(buffer, key) != NULL )
+				fputs(buffer, stdout);
+			memset(buffer, 0, MAX_LINE_SIZE);
+		}
+	}
+	else
+		fputs("ERROR: no input file found\n", stderr);
+}
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	if ( argc == 1 )
+		fputs("ERROR: not enough input given\n", stderr);
+	else
+	{
+		// char* key = argv[1];
+		FILE *file;
+		if ( argc == 2 )
+		{
+			// input in stdin
+			write_grepped(stdin, argv[1]);
+		}
+		else
+		{
+			// filename in argv[2]
+			file = fopen(argv[2],"r");
+			write_grepped(file, argv[1]);
+			fclose(file);
+		}
+	}
+
 	return 0;
 }
